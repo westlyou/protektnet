baa0e8d79c35c8fee2954a6249723e22e9f11ec9
Ernesto Garcia <ernesto.garcia@protektnet.com>
2018-01-19 16:42:25 -0600

    updating generic import module

---
+++ addons/bi_generic_import/__manifest__.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Odoo all import for Sales, Purchase, Invoice, Inventory, Picking, Product and Customer.',
+    'version': '11.0.0.0',
+    'sequence': 4,
+    'summary': 'Easy to import all odoo data i.e Invoice, Sale, Inventory, Purchase, Picking, Product and Customer.',
+    'price': 89,
+    'currency': 'EUR',
+    'category' : 'Extra Tools',
+    'description': """
+
+	BrowseInfo developed a new odoo/OpenERP module apps 
+	This module use for following easy import.
+	Import Stock from CSV and Excel file.
+    Import Stock inventory from CSV and Excel file.
+	Import inventory adjustment, import stock balance
+	Import opening stock balance from CSV and Excel file.
+	Import Sale order, Import sales order, Import Purchase order, Import purchases.
+	Import sale order line, import purchase order line, Import data, Import files, Import data from third party software
+	Import invoice from CSV, Import Bulk invoices easily.Import warehouse data,Import warehouse stock.Import product stock.
+	Invoice import from CSV, Invoice line import from CSV, Sale import, purchase import
+	Inventory import from CSV, stock import from CSV, Inventory adjustment import, Opening stock import. 
+	Import product from CSV, Import customer from CSV, Product Import,Customer import, Odoo CSV bridge,Import CSV brige on Odoo.Import csv data on odoo.All import, easy import, Import odoo data, Import CSV files, Import excel files 
+	Import tools Odoo, Import reports, import accounting data, import sales data, import purchase data, import data in odoo, import record, Import inventory.import data on odoo,Import data from Excel, Import data from CSV.Odoo Import Data
+
+	-Import product from CSV and Excel file.
+	-Import product from Excel and CSV file.
+	-Import variant from CSV and Excel file.
+	-Import variant from Excel and CSV file.
+	-Import product variant from CSV and Excel file.
+	-Import product variant from Excel and CSV file.
+
+	-Import item from CSV and Excel file.
+	-Import item from Excel and CSV file.
+
+	-Import partner from CSV and Excel file.
+	-Import vendor from csv and excel file
+	-Import partner from Excel and CSV file.
+	-Import contact from CSV and Excel file.
+	-Import contact from Excel and CSV file.
+	-Import Customer from CSV and Excel file.
+	-Import customer from Excel and CSV file.
+	-Import Supplier from CSV and Excel file.
+	-Import supplier from Excel and CSV file.
+
+	item import from csv, product import from csv, catelog import from csv, contact import from csv, partner import from csv, customer import from csv, supplier import from csv,variant import from csv, product variant import from csv, vendor import from csv
+
+	item import from excel, product import from excel, catelog import from excel, contact import from excel, partner import from excel, customer import from excel, supplier import from excel,variant import from excel, product variant import from excel, vendor import from excel
+
+	item import csv, product import csv, catelog import csv, contact import csv, partner import csv, customer import csv, supplier import csv,variant import csv, product variant import csv, vendor import csv
+
+	item import excel, product import excel, catelog import excel, contact import excel, partner import excel, customer import excel, supplier import excel,variant import excel, product variant import excel, vendor import excel
+    """,
+    'author': 'BrowseInfo',
+    'website': '',
+    'depends': ['base', 'sale_management', 'account', 'purchase', 'stock','product_expiry'],
+    'data': [
+             "views/account_invoice.xml",
+             "views/purchase_invoice.xml",
+             "views/sale.xml",
+             "views/stock_view.xml",
+             "views/product_view.xml",
+             "views/partner.xml",
+             "views/picking_view.xml"
+             ],
+	'qweb': [
+		],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'images':['static/description/Banner.png'],
+
+
+
+}
