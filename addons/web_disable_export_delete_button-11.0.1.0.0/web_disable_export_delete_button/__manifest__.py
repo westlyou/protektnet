bf2401dc3e6d9bf5a291c50a2eb58a72410411e4
jleal-pnet <34519698+jleal-pnet@users.noreply.github.com>
2018-03-28 11:48:43 -0600

    Add files via upload

---
+++ addons/web_disable_export_delete_button-11.0.1.0.0/web_disable_export_delete_button/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 InfoTerra (<http://www.infoterra.hr>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Web Disable Export and Delete Button',
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'InfoTerra',
+    'website': 'http://www.infoterra.hr',
+    'category': 'Web',
+    'depends': ['web'],
+    'data': [
+        'security/groups.xml',
+        'templates/assets.xml',
+    ],
+    'installable': True,
+}
