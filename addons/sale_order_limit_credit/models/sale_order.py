8c270456756373029a3f474cb77fb9bb6e74b597
Sarai Osorio <sarai_anai@hotmail.com>
2018-11-21 09:43:33 -0600

    [FIX][sale_order_limit_credit]

--- addons/sale_order_limit_credit/models/sale_order.py
+++ addons/sale_order_limit_credit/models/sale_order.py
@@ -19,18 +19,19 @@ class SaleOrder(models.Model):
             invoice_due = self.env['account.invoice'].search([
                 ('partner_id', '=', rec.partner_id.id),
                 ('state', '=', 'open')])
-            if invoice_due:
-                amount_credit = sum(invoice_due.mapped('residual'))
-                days_passed = [(fields.Date.from_string(
-                    fields.Date.today()) - fields.Date.from_string(date)
-                ).days for date in invoice_due.mapped('date_due')]
-                days_company = rec.company_id.credit_days_limit
-                days_partner = rec.partner_id.credit_days_limit
-                days_limit = (
-                    days_company if days_company == days_partner else days_partner)
-                dates = any(day > days_limit for day in days_passed)
-                if rec.partner_id.credit_limit < amount_credit or dates:
-                    rec.blocked_client = True
+            amount_credit = sum(invoice_due.mapped('residual'))
+            days_passed = [(
+                fields.Date.from_string(fields.Date.today()) -
+                fields.Date.from_string(date)).days
+                for date in invoice_due.mapped('date_due') if date is not False
+            ]
+            days_company = rec.company_id.credit_days_limit
+            days_partner = rec.partner_id.credit_days_limit
+            days_limit = (
+                days_company if days_company == days_partner else days_partner)
+            dates = any(day > days_limit for day in days_passed)
+            if rec.partner_id.credit_limit < amount_credit or dates:
+                rec.blocked_client = True
 
     @api.multi
     def action_unlocking(self):
