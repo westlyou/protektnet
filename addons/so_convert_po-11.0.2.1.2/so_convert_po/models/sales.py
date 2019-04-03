6c69b9d709e573027dee2c18de6cc3d29febd956
Grupo Censere <45492662+GpoCensere@users.noreply.github.com>
2019-04-03 15:31:46 -0600

    so_convert_po
    currency from so

--- addons/so_convert_po-11.0.2.1.2/so_convert_po/models/sales.py
+++ addons/so_convert_po-11.0.2.1.2/so_convert_po/models/sales.py
@@ -42,6 +42,8 @@ class SaleOrder(models.Model):
             'state': default_so_state,
             'x_studio_field_jaeoa': self.partner_id.id,
             'partner_id': self.get_partner_id(),
+            'currency_id': self.get_partner_id(
+            ).property_purchase_currency_id.id,
             'sale_order_id': self.id,
             'x_start_date': self.x_studio_field_nJisW,
             'x_end_date': self.	x_studio_field_vqN4P,
