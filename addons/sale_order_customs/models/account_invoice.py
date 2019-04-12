6b013d61f4cd4b96a3e081e8fd1478ba44944b39
Grupo Censere <sarai.osorio@grupocensere.com>
2019-04-12 08:56:52 -0500

    [REF][sale_order_customs]po automatic

--- addons/sale_order_customs/models/account_invoice.py
+++ addons/sale_order_customs/models/account_invoice.py
@@ -8,14 +8,14 @@ from odoo import _, api, fields, models
 class account_invoice(models.Model):
     _inherit = 'account.invoice'
 
-    @api.multi
-    def _l10n_mx_edi_retry(self):
-        invoice_po = self.invoice_line_ids.mapped('sale_line_ids').mapped(
-            'order_id').mapped(
-            'auto_purchase_order_id').sudo().invoice_ids.filtered(
-            lambda x: x.state == 'draft')
-        invoice_po.sudo().write({
-            'reference': self.number,
-        })
-        invoice_po.sudo().action_invoice_open()
-        return super(account_invoice, self)._l10n_mx_edi_retry()
+    # @api.multi
+    # def _l10n_mx_edi_retry(self):
+    #     invoice_po = self.invoice_line_ids.mapped('sale_line_ids').mapped(
+    #         'order_id').mapped(
+    #         'auto_purchase_order_id').sudo().invoice_ids.filtered(
+    #         lambda x: x.state == 'draft')
+    #     invoice_po.sudo().write({
+    #         'reference': self.number,
+    #     })
+    #     invoice_po.sudo().action_invoice_open()
+    #     return super(account_invoice, self)._l10n_mx_edi_retry()
